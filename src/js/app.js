457fd8fa109ed7403eb7e7c02585136f0abca64c
ConstanzaGarcia <o.conii.o@gmail.com>
2018-08-29 14:01:12 -0300

    js documentos

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,12 @@ setTimeout(function hide() {
   $('#pantallaUno').hide('fast');
   document.getElementById('contenedorPortafolioGeneral').style.display = 'block';
 }, 3500);
+
+
+function aProyectos() {
+  location.href = "#contenedorProyectos";
+}
+
+function aMi() {
+  location.href = "#contenedorAboutMe";
+}
