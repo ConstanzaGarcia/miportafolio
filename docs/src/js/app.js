5482f7d8cedb74e31ee2871467e609e4baa5a2c5
ConstanzaGarcia <o.conii.o@gmail.com>
2018-09-08 00:54:10 -0300

    carpetas docs

--- docs/src/js/app.js
+++ docs/src/js/app.js
@@ -1,4 +1,33 @@
-setTimeout(function hide() {
+/* setTimeout(function hide() {
   $('#pantallaUno').hide('fast');
   document.getElementById('contenedorPortafolioGeneral').style.display = 'block';
-}, 3500);
+}, 3500); */
+
+/*Para llamar a cada sección del portafolio*/
+function aHome() {
+  location.href = "#home";
+}
+
+function aProyectos() {
+  location.href = "#contenedorProyectos";
+}
+
+function aMi() {
+  location.href = "#aboutMe";
+}
+
+function marketPlace() {
+  location.href = "#malaMadre";
+}
+
+function redSocial() {
+  location.href = "#celiBook";
+}
+
+function registroPersonas() {
+  location.href = "#ifBlanco";
+}
+
+function footer() {
+  location.href = "#contacto";
+}
