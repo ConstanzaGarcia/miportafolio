d09ce5d912aa01c994aebb3199b4428c436d615c
ConstanzaGarcia <o.conii.o@gmail.com>
2018-09-30 05:16:45 -0300

    cambios completos

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,16 @@
-/* setTimeout(function hide() {
-  $('#pantallaUno').hide('fast');
-  document.getElementById('contenedorPortafolioGeneral').style.display = 'block';
-}, 3500); */
+$(document).on("scroll", function () {
+    if
+    ($(document).scrollTop() > 86) {
+        $("#banner").addClass("shrink");
+    }
+    else {
+        $("#banner").removeClass("shrink");
+    }
+});
 
-/*Para llamar a cada sección del portafolio*/
-function aHome() {
-  location.href = "#home";
-}
-
-function aProyectos() {
-  location.href = "#contenedorProyectos";
-}
-
-function aMi() {
-  location.href = "#contenedorAboutMe";
-}
-
-function marketPlace() {
-  location.href = "#malaMadre";
-}
-
-function redSocial() {
-  location.href = "#celiBook";
-}
-
-function registroPersonas() {
-  location.href = "#ifBlanco";
-}
-
-function footer() {
-  location.href = "#contacto";
-}
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+        $(this).removeClass("hover");
+    }
+);
