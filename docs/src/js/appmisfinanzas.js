156627dc51d2ea5c49bccda52818bb16d7cd799b
ConstanzaGarcia <o.conii.o@gmail.com>
2018-09-30 13:53:46 -0300

    cambios en docs

---
+++ docs/src/js/appmisfinanzas.js
@@ -0,0 +1,11 @@
+/*para barra superior */
+
+$(document).on("scroll", function () {
+    if
+    ($(document).scrollTop() > 86) {
+        $("#banner").addClass("shrink");
+    }
+    else {
+        $("#banner").removeClass("shrink");
+    }
+});
